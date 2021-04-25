09b6de8b57f5f9025d475124f399e284a302cb99
Daniel Copley <djcopley@users.noreply.github.com>
2021-04-25 16:44:26 -0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Daniel Copley on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
